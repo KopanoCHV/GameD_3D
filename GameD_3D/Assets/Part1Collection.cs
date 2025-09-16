26b22eb7b9247a47409540df7823e99b27c6e1af
KopanoCHV <kopanokopano174@gmail.com>
2025-09-16 17:48:20 +0200

    Part1Col

--- GameD_3D/Assets/Part1Collection.cs
+++ GameD_3D/Assets/Part1Collection.cs
@@ -1,15 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+
 
 public class Part1Collection : MonoBehaviour
 {
     private int Part1 = 0;
 
+    public TextMeshProUGUI Part1text;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.transform.tag == "Part1")
         {
             Part1++;
+            Part1text.text = "Part1: " + Part1.ToString();
             Debug.Log(Part1);
             Destroy(other.gameObject);
         }
