60bfc326e51476720e611448bd316ec86ec59e22
KopanoCHV <kopanokopano174@gmail.com>
2025-10-16 16:05:37 +0200

    Damage Screen 2

--- GameD_3D/Assets/Scripts/PlayerStats.cs
+++ GameD_3D/Assets/Scripts/PlayerStats.cs
@@ -1,19 +1,44 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour
 {
     [SerializeField] private float maxHealth;
 
     private float currentHealth;
-
+    
+    [Header("Health Bar")]
     public HealthBar healthBar;
+
+    [Header("Damage Overlay")]
+    public Image overlay;
+    public float duration;
+    public float fadeSpeed;
+
+    private float durationTimer;
+
+    
 
     private void Start()
     {
         currentHealth = maxHealth;
 
         healthBar.SetSliderMax(maxHealth);
+        overlay.color = new Color(overlay.color.r, overlay.color.g, overlay.color.b, 0);
+    }
+    private void Update()
+    {
+        if (overlay.color.a > 0)
+        {
+            durationTimer += Time.deltaTime;
+            if (durationTimer > duration)
+            {
+                float tempAlpha = overlay.color.a;
+                tempAlpha -= Time.deltaTime * fadeSpeed;
+                overlay.color = new Color(overlay.color.r, overlay.color.g, overlay.color.b, tempAlpha);
+            }
+        }
     }
 
     public void TakeDamage(float damage)
@@ -25,5 +50,7 @@ public class PlayerStats : MonoBehaviour
           // print("Player Died");    //For dead
           SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //For Restarting the scene  
         }
+        durationTimer = 0;
+        overlay.color = new Color(overlay.color.r, overlay.color.g, overlay.color.b, 1);
     }
 }
--- GameD_3D/Assets/Scripts/WaterDamage.cs
+++ GameD_3D/Assets/Scripts/WaterDamage.cs
@@ -1,7 +1,10 @@
+using UnityEditor.Overlays;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WaterDamage : MonoBehaviour
 {
+
     public float damage;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -16,7 +19,8 @@ public class WaterDamage : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             other.GetComponent<PlayerStats>().TakeDamage(damage);
-            }
+            
+        }
     }
 
 
