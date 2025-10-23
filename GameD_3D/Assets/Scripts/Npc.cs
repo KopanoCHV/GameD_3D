34d24b471c049f8628fd7f54e7eceed8dfb093ce
Complimentursmile <musankululeko3@gmail.com>
2025-10-23 15:06:41 +0200

    npc

--- GameD_3D/Assets/Scripts/Npc.cs
+++ GameD_3D/Assets/Scripts/Npc.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -21,6 +21,109 @@ public class Npc : MonoBehaviour
         if (dialogueText.text == dialogue[index])
         {
             nextButton.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsClose = true;
+            if (!dialogueSpace.activeInHierarchy)
+            {
+                dialogueSpace.SetActive(true);
+                StartCoroutine(Typing());
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsClose = false;
+            zeroText();
+        }
+    }
+
+    public void Nextline()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        nextButton.SetActive(false);
+        if (index < dialogue.Length - 1)
+        {
+            index++;
+            dialogueText.text = "";
+            StartCoroutine(Typing());
+        }
+        else
+        {
+            zeroText();
+        }
+    }
+
+    public void zeroText()
+    {
+        dialogueText.text = "";
+        index = 0;
+        dialogueSpace.SetActive(false);
+    }
+
+    IEnumerator Typing()
+    {
+        isTyping = true;
+        foreach (char letter in dialogue[index].ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(wordSpeed);
+        }
+        isTyping = false;
+    }
+}*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Npc : MonoBehaviour
+{
+    public GameObject dialogueSpace;
+    public TextMeshProUGUI dialogueText;
+    public string[] dialogue;
+    private int index;
+
+    public GameObject nextButton;
+    public float wordSpeed;
+    public bool playerIsClose;
+    private bool isTyping = false;
+
+    [Header("Input Settings")]
+    public KeyCode nextKey = KeyCode.Space;
+    public KeyCode skipKey = KeyCode.Space;
+
+    void Update()
+    {
+        if (dialogueText.text == dialogue[index])
+        {
+            nextButton.SetActive(true);
+        }
+
+        // Handle keyboard input for dialogue
+        if (playerIsClose && dialogueSpace.activeInHierarchy)
+        {
+            // Next line when typing is complete
+            if (Input.GetKeyDown(nextKey) && !isTyping)
+            {
+                Nextline();
+            }
+
+            // Skip typing animation
+            if (Input.GetKeyDown(skipKey) && isTyping)
+            {
+                SkipTyping();
+            }
         }
     }
 
@@ -77,5 +180,13 @@ public class Npc : MonoBehaviour
             yield return new WaitForSeconds(wordSpeed);
         }
         isTyping = false;
+    }
+
+    private void SkipTyping()
+    {
+        StopAllCoroutines();
+        dialogueText.text = dialogue[index];
+        isTyping = false;
+        nextButton.SetActive(true);
     }
 }
