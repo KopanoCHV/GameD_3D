7b3300f38f3ef44709d29f222a1478ec64044ee5
KopanoCHV <kopanokopano174@gmail.com>
2025-11-05 13:02:09 +0200

    EnemyDamage

---
+++ GameD_3D/Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+//Natty GameDev (2022) #3 Player Health & Damage Effect: Let's Make a First Person Game in Unity!. [Online] Available at: https://www.youtube.com/watch?v=LugpgsMdLWw (Accessed: 16 October 2025) 
+
+public class EnemyDamage : MonoBehaviour
+{
+    public float damage;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<PlayerStats>().TakeDamage(damage);
+        }
+    }
+}
